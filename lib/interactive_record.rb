552d1e034a35df15332ec1c689f9e771705009a2
Alyssa Kim <alyssa.kim@gmail.com>
2018-04-17 11:06:15 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -23,7 +23,7 @@ class InteractiveRecord
   end
 
   def initialize(options = {})
-    binding.rpy
+    binding.pry
     options.each do |property, value|
       self.send("#{property} = ", value)
     end
