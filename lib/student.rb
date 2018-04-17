d24bb51e30b4a23db0eeb85cf4dfdc2641913141
Alyssa Kim <alyssa.kim@gmail.com>
2018-04-17 11:05:17 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,7 +4,9 @@ require 'interactive_record.rb'
 
 class Student < InteractiveRecord
 
+def attr_accessor 
   self.column_names.each do |column_name|
     attr_accessor column_name.to_sym
   end 
+end 
 end
