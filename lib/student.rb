323bcdabed19e5d08c8739aa869f376b1fa4462f
Alyssa Kim <alyssa.kim@gmail.com>
2018-04-17 11:39:10 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,11 +5,7 @@ require 'pry'
 
 class Student < InteractiveRecord
 
-def attr_accessor
-  #binding.pry
   self.column_names.each do |column_name|
-
-  binding.pry
     attr_accessor column_name.to_sym
   end
 end
