6ccdce909d04a85c33f90ee3cd355ecbaf503e4d
Alyssa Kim <alyssa.kim@gmail.com>
2018-04-17 12:33:10 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -65,6 +65,6 @@ class InteractiveRecord
   end
 
   def self.find_by(attribute_hash)
-    sql = "SELECT * FROM #{table_name} WHERE #{attribute_hash.keys} = #{attribute_hash.values}
+    sql = "SELECT * FROM #{table_name} WHERE #{attribute_hash.keys} = #{attribute_hash.values}"
   end
 end
